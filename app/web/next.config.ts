f183ca5ad376e5997d569135db90823bd3aed15f
Joe <54695266+jdboris@users.noreply.github.com>
2024-12-25 08:14:31 -0500

    Change from dotenv-cli to dotenv to get the PORT in next app

--- app/web/next.config.ts
+++ app/web/next.config.ts
@@ -1,4 +1,8 @@
 import type { NextConfig } from "next";
+import { config } from "dotenv";
+
+// Allow setting PORT in .env
+config({ override: true });
 
 const nextConfig: NextConfig = {
   /* config options here */
