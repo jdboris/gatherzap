f7b55687440634e02b340f53b66a97ee32ca6735
Joe <54695266+jdboris@users.noreply.github.com>
2024-12-25 08:38:27 -0500

    Change run.sh to allow running in production mode

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,6 @@
-docker compose --progress=plain -p gatherzap --profile development -f docker-compose.yml -f docker-compose.dev.yml build &&
-  docker compose --progress=plain -p gatherzap --profile development -f docker-compose.yml -f docker-compose.dev.yml up --watch
+source .env
+
+docker compose --progress=plain -p gatherzap --profile $ENVIRONMENT -f docker-compose.yml $([ "$ENVIRONMENT" = "development" ] && echo "-f docker-compose.dev.yml") build &&
+  docker compose --progress=plain -p gatherzap --profile $ENVIRONMENT -f docker-compose.yml $([ "$ENVIRONMENT" = "development" ] && echo "-f docker-compose.dev.yml") up --watch
 
 $SHELL
