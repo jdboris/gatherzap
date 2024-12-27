be6aa08b8fa317b57241e43aa4dfc1c42d0838d4
Joe <54695266+jdboris@users.noreply.github.com>
2024-12-27 10:40:24 -0500

    Fix failure to resolve @ module path alias inside containers

--- app/web/next.config.ts
+++ app/web/next.config.ts
@@ -1,11 +1,23 @@
 import type { NextConfig } from "next";
 import { config } from "dotenv";
 
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 // Allow setting PORT in .env
 config({ override: true });
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.join(__dirname, "./src"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
