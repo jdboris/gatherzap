99e6d104999ca91cacd6925b579a86cdd5ccdf5a
Joe <54695266+jdboris@users.noreply.github.com>
2024-12-31 09:11:17 -0500

    Give clerk secret key env var to app service

--- app/web/@types/env.d.ts
+++ app/web/@types/env.d.ts
@@ -4,6 +4,7 @@ declare global {
       ENVIRONMENT: "production" | "development";
       PORT: string;
       NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: string;
+      CLERK_SECRET_KEY: string;
       NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: string;
       NEXT_PUBLIC_COMING_SOON_MODE: string;
       NEXT_PUBLIC_ADMIN_EMAIL: string;
