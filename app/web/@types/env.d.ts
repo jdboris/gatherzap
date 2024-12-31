acf28e7db4ee989840173b9cae0e3e3e5f09412c
Joe <54695266+jdboris@users.noreply.github.com>
2024-12-31 00:52:45 -0500

    Add missing env vars to env.d.ts

--- app/web/@types/env.d.ts
+++ app/web/@types/env.d.ts
@@ -1,8 +1,11 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      ENVIRONMENT: "production" | "development";
+      PORT: string;
       NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: string;
       NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: string;
+      NEXT_PUBLIC_COMING_SOON_MODE: string;
     }
   }
 }
