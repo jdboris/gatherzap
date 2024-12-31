f3f1a71a03193c7a6c638e01d2490fdfbc566370
Joe <54695266+jdboris@users.noreply.github.com>
2024-12-31 00:55:16 -0500

    Tweak coming soon overlay modal design

--- app/web/src/components/coming-soon-overlay.tsx
+++ app/web/src/components/coming-soon-overlay.tsx
@@ -1,7 +1,7 @@
 export default function ComingSoonOverlay() {
   return (
     <div className="backdrop-blur-sm fixed top-0 left-0 flex items-center justify-center w-full h-full">
-      <div className="rounded-xl p-7 max-w-96 text-center bg-white">
+      <div className="rounded-xl p-7 max-w-96 drop-shadow-md text-center bg-white">
         <div className=" text-2xl">Coming Soon...</div>
         <div>Gatherzap is currently under development.</div>
         <div>Stay tuned for great things in early 2025!</div>
