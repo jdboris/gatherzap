50d0d78aa29299b3c6bd07dfe3a0b75a1fbf7c9a
Joe <54695266+jdboris@users.noreply.github.com>
2024-12-31 05:29:20 -0500

    Improve coming soon overlay

--- app/web/@types/env.d.ts
+++ app/web/@types/env.d.ts
@@ -6,6 +6,8 @@ declare global {
       NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: string;
       NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: string;
       NEXT_PUBLIC_COMING_SOON_MODE: string;
+      NEXT_PUBLIC_ADMIN_EMAIL: string;
+      NEXT_PUBLIC_SOCIAL_LINK: string;
     }
   }
 }
--- app/web/src/components/coming-soon-overlay.tsx
+++ app/web/src/components/coming-soon-overlay.tsx
@@ -1,17 +1,32 @@
+import { FaLinkedin } from "react-icons/fa";
+
 export default function ComingSoonOverlay() {
   return (
-    <div className="backdrop-blur-sm fixed top-0 left-0 flex items-center justify-center w-full h-full">
-      <div className="rounded-xl p-7 max-w-96 drop-shadow-md text-center bg-white">
-        <div className=" text-2xl">Coming Soon...</div>
+    <div className="backdrop-blur-sm fixed top-0 left-0 flex items-center justify-center w-full h-full font-light">
+      <div className="rounded-xl p-7 max-w-96 drop-shadow-md gap flex flex-col gap-1 text-center bg-white">
+        <div className="text-2xl">Coming Soon...</div>
         <div>Gatherzap is currently under development.</div>
         <div>Stay tuned for great things in early 2025!</div>
+        <div>
+          Follow for updates:{" "}
+          <a
+            href={process.env.NEXT_PUBLIC_SOCIAL_LINK}
+            target="_blank"
+            className="inline-flex align-middle"
+          >
+            <FaLinkedin />
+          </a>
+        </div>
         <small>
           <em>
-            Contact{" "}
-            <a className="text-green" href="mailto:josephdboris@gmail.com">
-              josephdboris@gmail.com
-            </a>{" "}
-            for more info.
+            For inquiries, contact{" "}
+            <a
+              className="text-green"
+              href={`mailto:${process.env.NEXT_PUBLIC_ADMIN_EMAIL}`}
+            >
+              {process.env.NEXT_PUBLIC_ADMIN_EMAIL}
+            </a>
+            .
           </em>
         </small>
       </div>
