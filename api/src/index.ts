5a72df38890c7557aae792147faf7cb5cae85e72
Joe <54695266+jdboris@users.noreply.github.com>
2025-01-01 08:54:52 -0500

    Decouple coming soon mode from api routes more

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,17 +5,18 @@ import { COMING_SOON_MODE } from "./utils/feature-flags";
 const app = express();
 const { PORT, NODE_ENV } = process.env;
 
+if (!COMING_SOON_MODE) {
+  app.use(/.*/, (_: Request, res: Response) => {
+    res.status(404).send("This route does not exist.");
+  });
+}
+
 if (NODE_ENV == "development") {
   app.use(requestLogger);
 }
 
 app.use(express.json());
 
-if (!COMING_SOON_MODE) {
-  app.get("/", (_: Request, res: Response) => {
-    res.send("hello world");
-  });
-}
 
 app.listen(PORT, () => {
   console.log("NODE_ENV: ", NODE_ENV);
